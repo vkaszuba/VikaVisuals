6f067ade9b5e028673198089ef486d0c8192829a
vkaszuba <vikasz@live.com>
2022-01-12 00:52:30 -0800

    Added JS to navigate between pages

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,10 +1,25 @@
 
-// Redirects main Design button to its portfolio page
-document.getElementById("designCard").onclick = function () {
+// Redirects to landing page
+document.getElementById("navLogo").onclick = function () {
+  location.href = "./home.html";
+};
+
+// Redirects to design page
+document.getElementById("designPage").onclick = function () {
   location.href = "./design.html";
 };
 
-// Redirects main Coding button to its portfolio page
-document.getElementById("codeCard").onclick = function () {
+// Redirects to coding page
+document.getElementById("codingPage").onclick = function () {
   location.href = "./coding.html";
+};
+
+// Redirects to resume page
+document.getElementById("resumePage").onclick = function () {
+  location.href = "./resume.html";
+};
+
+// Redirects to contact page
+document.getElementById("contactPage").onclick = function () {
+  location.href = "./contact.html";
 };
