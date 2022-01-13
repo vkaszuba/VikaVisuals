749cf30c1c7c27e7ce045e2f9b6f9beca0888148
vkaszuba <vikasz@live.com>
2022-01-12 20:58:24 -0800

    added animated tablet to coging page

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -24,19 +24,32 @@ document.getElementById("contactPage").onclick = function () {
 
 
 
-var images = ["./assets/images/coding/ct-01.png",
+var chickenImages = ["./assets/images/coding/ct-01.png",
 "./assets/images/coding/ct-02.png",
 "./assets/images/coding/ct-03.png",
 "./assets/images/coding/ct-04.png"];
+
+var trippinImages = ["./assets/images/coding/wt-01.png",
+"./assets/images/coding/wt-02.png",
+"./assets/images/coding/wt-03.png",
+"./assets/images/coding/wt-04.png"];
+
 var index = 0;
 var ctImages = document.getElementById("chickenTinder");
+var wtImages = document.getElementById("weTrippin");
 var i = 0;
+
+
 setInterval(function() {
     if (++i % 2) {
         ctImages.style.opacity = 0;
+        wtImages.style.opacity = 0;
     } else {
-        ctImages.src = images[index = (index + 1) % images.length];
+        ctImages.src = chickenImages[index = (index + 1) % chickenImages.length];
         ctImages.style.opacity = 1;
+
+        wtImages.src = trippinImages[index = (index + 1) % trippinImages.length];
+        wtImages.style.opacity = 1;
     }
 }, 4000);
 
