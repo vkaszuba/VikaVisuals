469cf8726a09be036d021a9d15002ba00709de30
vkaszuba <vikasz@live.com>
2022-01-12 19:10:07 -0800

    Added design and coding pages

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -3,23 +3,43 @@
 document.getElementById("navLogo").onclick = function () {
   location.href = "./home.html";
 };
-
 // Redirects to design page
 document.getElementById("designPage").onclick = function () {
   location.href = "./design.html";
 };
-
 // Redirects to coding page
 document.getElementById("codingPage").onclick = function () {
   location.href = "./coding.html";
 };
-
 // Redirects to resume page
 document.getElementById("resumePage").onclick = function () {
   location.href = "./resume.html";
 };
-
 // Redirects to contact page
 document.getElementById("contactPage").onclick = function () {
   location.href = "./contact.html";
 };
+
+
+
+
+
+
+// function designLink() {
+//   document.getElementById("designPage").onclick = function () {
+//   location.href = "./design.html";
+//   }
+// };
+
+// function transition () {
+//     var video = document.getElementById('transition')
+//     video.play();
+//     video.addEventListener('ended', function() {
+//       video.style.display = "none";
+//     })
+//   };
+
+// function designPage() {
+//   transition();
+//   designLink();
+// }
