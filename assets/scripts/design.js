4afe8043f269db60ea47fb6cdb6dfb3cef8bfc85
vkaszuba <vikasz@live.com>
2022-01-13 20:56:04 -0800

    Added moving logo images to design page

--- assets/scripts/design.js
+++ assets/scripts/design.js
@@ -32,15 +32,30 @@ var flyerTwoImages = ["./assets/images/design/tum.png",
     "./assets/images/design/space02.png",
     "./assets/images/design/foreplay.png"];
 
+var logoOneImages = ["./assets/images/design/logo-vv.png",
+    "./assets/images/design/logo-vamp.png",
+    "./assets/images/design/logo-leaf.png"];
+
+var logoTwoImages = ["./assets/images/design/logo-vk.png",
+    "./assets/images/design/logo-tum.png",
+    "./assets/images/design/logo-sfqn.png"];
+
 
 var flyerOne = document.getElementById("flyerOne");
 var flyerTwo = document.getElementById("flyerTwo");
 
+var logoOne = document.getElementById("logoOne");
+var logoTwo = document.getElementById("logoTwo");
+
 var flyerOneIndex = 0;
 var flyerTwoIndex = 0;
+var logoOneIndex = 0;
+var logoTwoIndex = 0;
+
 var d = 0;
 var e = 0;
-
+var f = 0;
+var g = 0;
 
 
 function flyerOneTimer() {
@@ -61,7 +76,27 @@ function flyerTwoTimer() {
     }
 };
 
+function logoOneTimer() {
+    if (++f % 2) {
+        logoOne.style.opacity = 0;
+    } else {
+        logoOne.src = logoOneImages[logoOneIndex = (logoOneIndex + 1) % logoOneImages.length];
+        logoOne.style.opacity = 1;
+    }
+};
+
+function logoTwoTimer() {
+    if (++g % 2) {
+        logoTwo.style.opacity = 0;
+    } else {
+        logoTwo.src = logoTwoImages[logoTwoIndex = (logoTwoIndex + 1) % logoTwoImages.length];
+        logoTwo.style.opacity = 1;
+    }
+};
+
 setInterval(function () {
     flyerOneTimer();
     flyerTwoTimer();
+    logoOneTimer();
+    logoTwoTimer()
 }, 4000);
