80e59d2a6d6aedffa9149762e0727b4e911b68de
vkaszuba <vikasz@live.com>
2022-01-15 22:07:55 -0800

    Added pluto section to game art page

--- assets/scripts/gameart.js
+++ assets/scripts/gameart.js
@@ -12,6 +12,14 @@ var enviroImages = ["./assets/images/design/RA_01.png",
     "./assets/images/design/WF_03.png",
     "./assets/images/design/WF_04.png"];
 
+    var plutoImages = ["./assets/images/design/Pluto_01.png",
+    "./assets/images/design/Pluto_02.png",
+    "./assets/images/design/Pluto_03.png",
+    "./assets/images/design/Pluto_04.png",
+    "./assets/images/design/Pluto_05.png",
+    "./assets/images/design/Pluto_06.png",
+    "./assets/images/design/Pluto_07.png"];
+
     var professionalImages = ["./assets/images/design/Nokia_01.png",
     "./assets/images/design/Nokia_02.png",
     "./assets/images/design/Nokia_03.png",
@@ -20,13 +28,16 @@ var enviroImages = ["./assets/images/design/RA_01.png",
     "./assets/images/design/HP_03.png"];
 
     var enviro = document.getElementById("enviro");
+    var pluto = document.getElementById("pluto");
     var professional = document.getElementById("professional");
 
     var enviroIndex = 0;
+    var plutoIndex = 0;
     var professionalIndex = 0;
 
     var j = 0;
     var k = 0;
+    var l = 0;
 
     function enviroTimer() {
         if (++j % 2) {
@@ -37,8 +48,17 @@ var enviroImages = ["./assets/images/design/RA_01.png",
         }
     };
 
-    function professionalTimer() {
+    function plutoTimer() {
         if (++k % 2) {
+            pluto.style.opacity = 0;
+        } else {
+            pluto.src = plutoImages[plutoIndex = (plutoIndex + 1) % plutoImages.length];
+            pluto.style.opacity = 1;
+        }
+    };
+
+    function professionalTimer() {
+        if (++l % 2) {
             professional.style.opacity = 0;
         } else {
             professional.src = professionalImages[professionalIndex = (professionalIndex + 1) % professionalImages.length];
@@ -48,6 +68,7 @@ var enviroImages = ["./assets/images/design/RA_01.png",
 
     setInterval(function () {
         enviroTimer();
+        plutoTimer();
         professionalTimer();
     }, 4000);
     
