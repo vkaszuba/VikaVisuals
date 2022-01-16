1256d8031aa2334f099d5e3762bc0f37479d98a8
vkaszuba <vikasz@live.com>
2022-01-15 21:54:47 -0800

    Added images and modal for professionals

--- assets/scripts/gameart.js
+++ assets/scripts/gameart.js
@@ -12,11 +12,21 @@ var enviroImages = ["./assets/images/design/RA_01.png",
     "./assets/images/design/WF_03.png",
     "./assets/images/design/WF_04.png"];
 
+    var professionalImages = ["./assets/images/design/Nokia_01.png",
+    "./assets/images/design/Nokia_02.png",
+    "./assets/images/design/Nokia_03.png",
+    "./assets/images/design/HP_01.png",
+    "./assets/images/design/HP_02.png",
+    "./assets/images/design/HP_03.png"];
+
     var enviro = document.getElementById("enviro");
+    var professional = document.getElementById("professional");
 
     var enviroIndex = 0;
+    var professionalIndex = 0;
 
     var j = 0;
+    var k = 0;
 
     function enviroTimer() {
         if (++j % 2) {
@@ -27,7 +37,17 @@ var enviroImages = ["./assets/images/design/RA_01.png",
         }
     };
 
+    function professionalTimer() {
+        if (++k % 2) {
+            professional.style.opacity = 0;
+        } else {
+            professional.src = professionalImages[professionalIndex = (professionalIndex + 1) % professionalImages.length];
+            professional.style.opacity = 1;
+        }
+    };
+
     setInterval(function () {
         enviroTimer();
+        professionalTimer();
     }, 4000);
     
